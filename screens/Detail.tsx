6b20c493329b3e5d42354da91024b14d4872e487
DevelopJKong <ybaram40@gmail.com>
2022-07-25 07:45:46 +0900

    #3.23 Detail Screen part Three 완료

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -1,11 +1,44 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
+import { Dimensions, StyleSheet } from "react-native";
 import styled from "styled-components/native";
 import { Movie } from "../api";
 import Poster from "../components/Poster";
+import { LinearGradient } from "expo-linear-gradient";
+import { makeImgPath } from "../utils";
+import { BLACK_COLOR } from "../colors";
+
+const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
+`;
+
+const Header = styled.View`
+  height: ${SCREEN_HEIGHT / 4}px;
+  justify-content: flex-end;
+  padding: 0px 20px;
+`;
+
+const Background = styled.Image``;
+
+const Column = styled.View`
+  flex-direction: row;
+`;
+
+const Title = styled.Text`
+  color: white;
+  font-size: 18px;
+  align-self: flex-end;
+  width: 80%;
+  margin-left: 15px;
+  font-weight: 500;
+`;
+
+const Overview = styled.Text`
+  color: ${(props) => props.theme.textColor};
+  margin-top: 30px;
+  padding: 0px 20px;
 `;
 
 type RootStackParamList = {
@@ -22,13 +55,31 @@ const Detail: React.FC<DetailScreenProps> = ({
     setOptions({
       title:
         "original_title" in params
-          ? params.original_title
-          : params.original_name,
+          ? "Movie"
+          : "TV Show",
     });
   }, []);
   return (
     <Container>
-      <Poster path={params.poster_path || ""} />
+      <Header>
+        <Background
+          style={StyleSheet.absoluteFill}
+          source={{ uri: makeImgPath(params.backdrop_path || "") }}
+        />
+        <LinearGradient
+          colors={["transparent", BLACK_COLOR]}
+          style={StyleSheet.absoluteFill}
+        />
+        <Column>
+          <Poster path={params.poster_path || ""} />
+          <Title>
+            {"original_title" in params
+              ? params.original_title
+              : params.original_name}
+          </Title>
+        </Column>
+      </Header>
+      <Overview>{params.overview}</Overview>
     </Container>
   );
 };
