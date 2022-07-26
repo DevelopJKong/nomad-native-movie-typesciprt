47e8e705bc23f646a1e7b26f1e48949be77b424a
DevelopJKong <ybaram40@gmail.com>
2022-07-27 07:32:07 +0900

    #3.24 Detail Screen part Four 완료

--- screens/Detail.tsx
+++ screens/Detail.tsx
@@ -2,61 +2,67 @@ import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import React, { useEffect } from "react";
 import { Dimensions, StyleSheet } from "react-native";
 import styled from "styled-components/native";
-import { Movie } from "../api";
-import Poster from "../components/Poster";
 import { LinearGradient } from "expo-linear-gradient";
+import { Movie, moviesApi, TV, tvApi } from "../api";
+import Poster from "../components/Poster";
 import { makeImgPath } from "../utils";
 import { BLACK_COLOR } from "../colors";
+import { useQuery } from "react-query";
 
 const { height: SCREEN_HEIGHT } = Dimensions.get("window");
 
 const Container = styled.ScrollView`
   background-color: ${(props) => props.theme.mainBgColor};
 `;
-
 const Header = styled.View`
   height: ${SCREEN_HEIGHT / 4}px;
   justify-content: flex-end;
   padding: 0px 20px;
 `;
-
 const Background = styled.Image``;
-
 const Column = styled.View`
   flex-direction: row;
+  width: 80%;
 `;
-
 const Title = styled.Text`
   color: white;
-  font-size: 18px;
+  font-size: 36px;
   align-self: flex-end;
-  width: 80%;
   margin-left: 15px;
   font-weight: 500;
 `;
-
 const Overview = styled.Text`
   color: ${(props) => props.theme.textColor};
-  margin-top: 30px;
+  margin-top: 20px;
   padding: 0px 20px;
 `;
-
 type RootStackParamList = {
-  Detail: Movie;
+  Detail: Movie | TV;
 };
-
 type DetailScreenProps = NativeStackScreenProps<RootStackParamList, "Detail">;
-
 const Detail: React.FC<DetailScreenProps> = ({
   navigation: { setOptions },
   route: { params },
 }) => {
+  const { isLoading: moviesLoading, data: moviesData } = useQuery(
+    ["movies", params.id],
+    moviesApi.detail,
+    {
+      enabled: "original_title" in params,
+    }
+  );
+  const { isLoading: tvLoading, data: tvData } = useQuery(
+    ["tv", params.id],
+    tvApi.detail,
+    {
+      enabled: "original_name" in params,
+    }
+  );
+  console.log("movies", moviesData);
+  console.log("tv", tvData);
   useEffect(() => {
     setOptions({
-      title:
-        "original_title" in params
-          ? "Movie"
-          : "TV Show",
+      title: "original_title" in params ? "Movie" : "TV Show",
     });
   }, []);
   return (
